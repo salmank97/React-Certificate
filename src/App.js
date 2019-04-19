c8e26cbb36d71aac4204e3c3d8351b6ff5293615
salmank97 <salman5@live.com>
2019-04-19 16:26:10 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Container,Row,Col,Visible,Hidden} from 'react-grid-system';
+import inventLogo from './assets/invent-logo.png'
+import IBALogo from './assets/IBA-logo.png'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {/* <header className="App-header">
+          Hello
+        </header> */}
+        <div>
+          <div className="cert-border">
+            <Container>
+              <Row>
+                <Col  md={3} xs={4} sm={4}>
+                  <img className="header-logo-center" src={IBALogo}/>
+                </Col>
+                <Col  md={1} sm={0} xs={0}>
+                  
+                </Col>
+                <Col className="text-center" md={4} xs={4} sm={4}>
+                  <img className="header-logo-left" src={inventLogo}/>
+                </Col>
+                <Col  md={1} sm={0} xs={0}>
+                  
+                </Col>
+                <Col  md={3} xs={4} sm={4}>
+                  Test3
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </div>
       </div>
     );
   }
